03564de1752d91b9ddb699713aedea691ecccfd6
sosamourai <ms.elm.amiens@gmail.com>
2025-11-24 14:50:05 +0100

    commit ex 4

--- ex1.py
+++ ex1.py
@@ -1,7 +1,7 @@
 from random import *
 def simulation_des_lancers(nb_lancers: int = 100):
     resultats = []
-    for _ in range(nb_lancers):
+    for i in range(nb_lancers):
         lancer = random.randint(1, 6)
-        resultats.append(lancer)
-    return resultats
+        resultats.append(lancer)    
+        return resultats
